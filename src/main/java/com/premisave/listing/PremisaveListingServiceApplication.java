7f16ed1d48b8e52c37fe9b61171d962595382883
Bill Graham Peacemaker <billgrahamp@gmail.com>
2026-06-16 23:27:20 +0300

    Setting up project structure

---
+++ src/main/java/com/premisave/listing/PremisaveListingServiceApplication.java
@@ -0,0 +1,13 @@
+package com.premisave.listing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PremisaveListingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PremisaveListingServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/premisave/listing/PremisaveListingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.premisave.listing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PremisaveListingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
